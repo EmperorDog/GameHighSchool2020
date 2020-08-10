2cd6a931c3af02a977b95966e6e36583bb3d84ed
EmperorDog <protos1248@gmail.com>
2020-08-10 21:12:45 +0900

    커밋

---
+++ AfterSchoolTest/Assets/AfterSchool2/Scripts/BlueCube.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueCube : MonoBehaviour
+{
+
+    public float m_Speed = 5;
+
+    void Update()
+    {
+        var movement = Vector3.down * m_Speed * Time.deltaTime;
+        transform.position += movement;
+    }
+
+    public void OnPointerDownEvent()
+    {
+        GameManager.Instance.DamageLife();
+        Destroy(gameObject);
+        //클릭시 데미지
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "plane")
+        {
+            GameManager.Instance.AddScore();
+            Destroy(gameObject);
+            //바닥에 점수
+        }
+    }
+}
---
+++ AfterSchoolTest/Assets/AfterSchoolTest/Scripts/PlayerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public GameObject Particle;
+    public float Speed = 8;
+    void Update()
+    {
+        float xAxis = Input.GetAxisRaw("Horizontal");
+        float yAxis = Input.GetAxisRaw("Vertical");
+
+        float speed = 5f;
+
+        transform.position += new Vector3(xAxis, 0, yAxis) * Speed * Time.deltaTime;
+
+        RaycastHit hit;
+        Ray ray = new Ray();
+        ray.origin = transform.position;
+        ray.direction = Vector3.forward;
+        if (Physics.Raycast(ray, out hit, 10f, LayerMask.GetMask("1", "2"), QueryTriggerInteraction.Collide))
+        {
+            GameObject.Instantiate(Particle, hit.point, Quaternion.identity);
+        }
+        Debug.DrawLine(Vector3.zero, Vector3.zero + Vector3.forward * 100f, Color.red);
+    }
+}
